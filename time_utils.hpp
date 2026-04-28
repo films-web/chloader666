894eaa3ad124102c0336ac232229caf521cf30c7
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-04-28 17:26:38 +0300

    Update time_utils.hpp

--- time_utils.hpp
+++ time_utils.hpp
@@ -6,14 +6,20 @@
 #include <ctime>
 
 namespace TimeUtils {
-    inline std::string GetFormattedTime() {
+    static __forceinline std::string GetFormattedTime() {
         auto now = std::chrono::system_clock::now();
         std::time_t now_c = std::chrono::system_clock::to_time_t(now);
         std::tm parts;
+
         localtime_s(&parts, &now_c);
+
         std::stringstream timeStream;
         timeStream << std::put_time(&parts, "%I:%M:%S %p");
 
         return timeStream.str();
+    }
+
+    static __forceinline uint64_t GetUnixTimestamp() {
+        return static_cast<uint64_t>(std::time(nullptr));
     }
 }
