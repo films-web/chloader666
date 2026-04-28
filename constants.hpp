dd9a0a439566fb5d64a5e3c2e51436cfb04ded84
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-04-28 20:14:01 +0300

    Update constants.hpp

--- constants.hpp
+++ constants.hpp
@@ -5,6 +5,6 @@ namespace Constants {
     static __forceinline auto WsUrl() { return PCrypt("wss://ws.ch-sof2.online/connect"); }
     static __forceinline auto TargetExe() { return PCrypt("sof2mp.exe"); }
     static __forceinline auto IpcPipeName() { return PCrypt("\\\\.\\pipe\\CHPipe"); }
-    static __forceinline auto AesTransportKey() { return PCrypt("Ch34tH4r4m_S3cr3t_K3y_256B1t_!!!X"); }
+    static __forceinline auto AesTransportKey() { return PCrypt("Ch34tH4r4m_S3cr3t_K3y_256B1t_!!!"); }
     static __forceinline auto ClientSecret() { return PCrypt("baf0f0e65f1b2688b8d99cb628d21c9a07e8992174fe2f9cfc15cef7f92a6fb9"); }
 }
