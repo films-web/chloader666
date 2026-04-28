1e4fd145befcc73e511cbe9c30e4665f167160db
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-04-28 16:17:15 +0300

    yes

--- constants.hpp
+++ constants.hpp
@@ -6,4 +6,5 @@ namespace Constants {
     inline auto TargetExe() { return PCrypt("sof2mp.exe"); }
     inline auto IpcPipeName() { return PCrypt("\\\\.\\pipe\\CHPipe"); }
     inline auto AesTransportKey() { return PCrypt("Ch34tH4r4m_S3cr3t_K3y_256B1t_!!!X"); }
+    inline auto ClientSecret() { return PCrypt("baf0f0e65f1b2688b8d99cb628d21c9a07e8992174fe2f9cfc15cef7f92a6fb9"); }
 }
