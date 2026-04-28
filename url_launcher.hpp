cdc25879a8e21b1888b582701bb6bd6d1bd74bd1
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-04-28 18:24:41 +0300

    yes

--- url_launcher.hpp
+++ url_launcher.hpp
@@ -4,59 +4,63 @@
 #include <string>
 #include <thread>
 #include <chrono>
-
+#include <atomic>
 #include "constants.hpp"
 #include "event_bus.hpp"
+#include "poly_crypt.hpp"
 
-class UrlLauncher {
-private:
+namespace UrlLauncher {
     static inline HANDLE hMutex = NULL;
-
-public:
-    static void RegisterProtocol() {
+    static __forceinline void RegisterProtocol() {
         char exePath[MAX_PATH];
         GetModuleFileNameA(NULL, exePath, MAX_PATH);
+
         HKEY hKey;
-        std::string keyPath = "Software\\Classes\\cheatharam";
+        std::string keyPath = PCrypt("Software\\Classes\\cheatharam").c_str();
+
         if (RegCreateKeyExA(HKEY_CURRENT_USER, keyPath.c_str(), 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
-            std::string desc = "URL:CheatHaram Protocol";
-            RegSetValueExA(hKey, NULL, 0, REG_SZ, (const BYTE*)desc.c_str(), desc.length() + 1);
-            RegSetValueExA(hKey, "URL Protocol", 0, REG_SZ, (const BYTE*)"", 1);
+            std::string desc = PCrypt("URL:CheatHaram Protocol").c_str();
+            RegSetValueExA(hKey, NULL, 0, REG_SZ, (const BYTE*)desc.c_str(), (DWORD)desc.length() + 1);
+
+            std::string urlProtocol = PCrypt("URL Protocol").c_str();
+            RegSetValueExA(hKey, urlProtocol.c_str(), 0, REG_SZ, (const BYTE*)"", 1);
             RegCloseKey(hKey);
         }
-        std::string cmdKeyPath = keyPath + "\\shell\\open\\command";
+
+        std::string cmdKeyPath = keyPath + PCrypt("\\shell\\open\\command").c_str();
         if (RegCreateKeyExA(HKEY_CURRENT_USER, cmdKeyPath.c_str(), 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
-            std::string cmd = "\"" + std::string(exePath) + "\" \"%1\"";
-            RegSetValueExA(hKey, NULL, 0, REG_SZ, (const BYTE*)cmd.c_str(), cmd.length() + 1);
+            std::string cmd = std::string(PCrypt("\"").c_str()) + exePath + PCrypt("\" \"%1\"").c_str();
+            RegSetValueExA(hKey, NULL, 0, REG_SZ, (const BYTE*)cmd.c_str(), (DWORD)cmd.length() + 1);
             RegCloseKey(hKey);
         }
     }
 
-    static std::string ParseArgument(int argc, char* argv[]) {
+    static __forceinline std::string ParseArgument(int argc, char* argv[]) {
         if (argc > 1) {
             std::string inputArg(argv[1]);
-            std::string scheme = "cheatharam://";
+            std::string scheme = PCrypt("cheatharam://").c_str();
             if (inputArg.find(scheme) == 0) {
                 std::string serverAddress = inputArg.substr(scheme.length());
                 if (!serverAddress.empty() && serverAddress.back() == '/') serverAddress.pop_back();
                 return serverAddress;
             }
-            else {
-                return inputArg;
-            }
+            return inputArg;
         }
         return "";
     }
 
-    static bool ForwardIfAlreadyRunning(const std::string& serverAddress) {
-        hMutex = CreateMutexA(NULL, TRUE, "CheatHaram_SingleInstance_Mutex");
+    static __forceinline bool ForwardIfAlreadyRunning(const std::string& serverAddress) {
+        hMutex = CreateMutexA(NULL, TRUE, PCrypt("Global\\CH_Instance_Guard").c_str());
+
         if (GetLastError() == ERROR_ALREADY_EXISTS) {
-            if (WaitNamedPipeA("\\\\.\\pipe\\CHUrlPipe", 5000)) {
-                HANDLE hPipe = CreateFileA("\\\\.\\pipe\\CHUrlPipe", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+            std::string pipeName = PCrypt("\\\\.\\pipe\\CHUrlPipe").c_str();
+
+            if (WaitNamedPipeA(pipeName.c_str(), 2000)) {
+                HANDLE hPipe = CreateFileA(pipeName.c_str(), GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
                 if (hPipe != INVALID_HANDLE_VALUE) {
-                    std::string msg = serverAddress.empty() ? "WAKE_UP" : "CONNECT_IP:" + serverAddress;
+                    std::string msg = serverAddress.empty() ? PCrypt("WAKE_UP").c_str() : std::string(PCrypt("CONNECT_IP:").c_str()) + serverAddress;
                     DWORD bytesWritten;
-                    WriteFile(hPipe, msg.c_str(), msg.length(), &bytesWritten, NULL);
+                    WriteFile(hPipe, msg.c_str(), (DWORD)msg.length(), &bytesWritten, NULL);
                     CloseHandle(hPipe);
                 }
             }
@@ -65,9 +69,11 @@ public:
         return false;
     }
 
-    static std::thread StartPrimaryListener(EventBus& bus, std::atomic<bool>& isRunning) {
+    static __forceinline std::thread StartPrimaryListener(EventBus& bus, std::atomic<bool>& isRunning) {
         return std::thread([&bus, &isRunning]() {
-            HANDLE hPipe = CreateNamedPipeA("\\\\.\\pipe\\CHUrlPipe",
+            std::string pipeName = PCrypt("\\\\.\\pipe\\CHUrlPipe").c_str();
+
+            HANDLE hPipe = CreateNamedPipeA(pipeName.c_str(),
                 PIPE_ACCESS_INBOUND | FILE_FLAG_OVERLAPPED,
                 PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
                 1, 1024, 1024, 0, NULL);
@@ -122,8 +128,9 @@ public:
                                     SetForegroundWindow(hwnd);
                                 }
 
-                                if (msg.rfind("CONNECT_IP:", 0) == 0) {
-                                    std::string targetIp = msg.substr(11);
+                                std::string connectCmd = PCrypt("CONNECT_IP:").c_str();
+                                if (msg.rfind(connectCmd, 0) == 0) {
+                                    std::string targetIp = msg.substr(connectCmd.length());
                                     bus.Publish({ EventType::URL_CONNECT_REQUESTED, targetIp });
                                 }
                             }
@@ -133,10 +140,9 @@ public:
                     CloseHandle(connectOv.hEvent);
                 }
                 DisconnectNamedPipe(hPipe);
-
                 if (isRunning) std::this_thread::sleep_for(std::chrono::milliseconds(10));
             }
             CloseHandle(hPipe);
             });
     }
-};
+}
