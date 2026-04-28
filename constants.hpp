aebb9c6e24ed078cb88d33deb4ce6704e06c9ee5
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-04-28 16:52:31 +0300

    Update constants.hpp

--- constants.hpp
+++ constants.hpp
@@ -2,9 +2,9 @@
 #include "poly_crypt.hpp"
 
 namespace Constants {
-    inline auto WsUrl() { return PCrypt("wss://ws.ch-sof2.online/connect"); }
-    inline auto TargetExe() { return PCrypt("sof2mp.exe"); }
-    inline auto IpcPipeName() { return PCrypt("\\\\.\\pipe\\CHPipe"); }
-    inline auto AesTransportKey() { return PCrypt("Ch34tH4r4m_S3cr3t_K3y_256B1t_!!!X"); }
-    inline auto ClientSecret() { return PCrypt("baf0f0e65f1b2688b8d99cb628d21c9a07e8992174fe2f9cfc15cef7f92a6fb9"); }
+    static __forceinline auto WsUrl() { return PCrypt("wss://ws.ch-sof2.online/connect"); }
+    static __forceinline auto TargetExe() { return PCrypt("sof2mp.exe"); }
+    static __forceinline auto IpcPipeName() { return PCrypt("\\\\.\\pipe\\CHPipe"); }
+    static __forceinline auto AesTransportKey() { return PCrypt("Ch34tH4r4m_S3cr3t_K3y_256B1t_!!!X"); }
+    static __forceinline auto ClientSecret() { return PCrypt("baf0f0e65f1b2688b8d99cb628d21c9a07e8992174fe2f9cfc15cef7f92a6fb9"); }
 }
